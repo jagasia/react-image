151b6b783ebf10896348e21790d46468babfa90a
jagasia <rjagadeeswaran@Yahoo.com>
2022-03-24 15:27:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import Product from './components/product';
+import DisplayProducts from './components/display-products';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="jumbotron text-center">
+  <h1>React + Rest api with image Demo</h1>
+  <p>Done by JF027 and JF028 team!</p> 
+</div>
+  
+<div class="container">
+  <div class="row">
+    <div class="col-sm-4">
+      
+     
+    </div>
+    <div class="col-sm-4">
+      
+        <Product />
+    </div>
+    <div class="col-sm-4">
+      
+     
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-sm-4">
+      
+     
+    </div>
+    <div class="col-sm-4">
+      
+        <DisplayProducts />
+    </div>
+    <div class="col-sm-4">
+      
+     
+    </div>
+  </div>
+</div>
+
+
+
     </div>
   );
 }
